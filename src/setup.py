80ecb6a2316a3d5a26cea54ce939aabe99a57193
unknown <radwan@post.bgu.ac.il>
2024-03-10 17:52:58 +0200

    python package setup

---
+++ src/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
+
+#from setuptools import setup, find_packages
+
+# with open('README.md') as readme_file:
+#     README = readme_file.read()
+
+# with open('HISTORY.md') as history_file:
+#     HISTORY = history_file.read()
+
+# setup_args = dict(
+#     name='TuringMachineTutor',
+#     version='0.1.2',
+#     description='Turing machine',
+#     #long_description_content_type="text/markdown",
+#     #long_description=README + '\n\n' + HISTORY,
+#     license='BGU',
+#     packages=find_packages(),
+#     author='Group13',
+#     author_email='asd@gmail.com',
+#     # keywords=['Elastic', 'ElasticSearch', 'ElasticStack'],
+#     # url='https://github.com/ncthuc/elastictools',
+#     download_url=''
+# )
+
+# install_requires = [
+    
+# ]
+
+# if __name__ == '__main__':
+#     setup(**setup_args, install_requires=install_requires)
---
+++ src/turing_machine_tutor/__init__.py
@@ -0,0 +1,19 @@
+from . import Challenge
+from . import CombinedTuringMachine
+from . import configuration
+from . import IFTuringMachine
+from . import machine_run_state
+from . import TuringMachine
+from . import TuringMachineController
+from . import TuringMachineVisualizer
+
+__all__ = [
+    'Challenge',
+    'CombinedTuringMachine',
+    'configuration',
+    'IFTuringMachine',
+    'machine_run_state',
+    'TuringMachine',
+    'TuringMachineController',
+    'TuringMachineVisualizer'
+]
