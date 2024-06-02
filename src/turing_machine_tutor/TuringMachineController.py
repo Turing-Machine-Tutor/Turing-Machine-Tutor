860a3992ca8772bca372abaac5e55557587ea842
unknown <radwan@post.bgu.ac.il>
2024-06-03 00:00:22 +0300

    submissions_attempt2

--- src/turing_machine_tutor/TuringMachineController.py
+++ src/turing_machine_tutor/TuringMachineController.py
@@ -382,13 +382,13 @@ class TuringMachineController:
 
 
 
-def log_results(self, TM, spreadsheet_url):
-    # Authorize the Google Sheets API
-    auth.authenticate_user()
-    creds, _ = default()
-    gc = gspread.authorize(creds)
-    sheet = gc.open_by_url(spreadsheet_url).sheet1
-    # Get User ID
-    user_id = input("Please enter your ID number: ")
-    #"""Log the test results to Google Sheets."""
-    sheet.append_row([user_id, self.get_turing_machine(TM).__str__(), "Passed" if self.validate_turing_machineTA('0n1n') else "Failed"])
+    def log_results(self, TM, spreadsheet_url):
+        # Authorize the Google Sheets API
+        auth.authenticate_user()
+        creds, _ = default()
+        gc = gspread.authorize(creds)
+        sheet = gc.open_by_url(spreadsheet_url).sheet1
+        # Get User ID
+        user_id = input("Please enter your ID number: ")
+        #"""Log the test results to Google Sheets."""
+        sheet.append_row([user_id, self.get_turing_machine(TM).__str__(), "Passed" if self.validate_turing_machineTA('0n1n') else "Failed"])
